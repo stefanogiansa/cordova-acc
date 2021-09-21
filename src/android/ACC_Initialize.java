8092e68997702e74e425225f7c8ee6a816610af6
Stefano Giansante <giansante.stefano@konvergence.it>
2021-09-21 11:00:42 +0200

    initialize

--- src/android/ACC_Initialize.java
+++ src/android/ACC_Initialize.java
@@ -2,21 +2,38 @@ package com.konvergence.acc.cordova;
 
 import android.app.Application;
 import android.util.Log;
+
+import com.adobe.marketing.mobile.AdobeCallback;
 import com.adobe.marketing.mobile.CampaignClassic;
+import com.adobe.marketing.mobile.Identity;
+import com.adobe.marketing.mobile.InvalidInitException;
 import com.adobe.marketing.mobile.Lifecycle;
+import com.adobe.marketing.mobile.LoggingMode;
+import com.adobe.marketing.mobile.MobileCore;
+import com.adobe.marketing.mobile.Signal;
+import com.adobe.marketing.mobile.UserProfile;
 
 public class ACC_Initialize extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        
         MobileCore.setApplication(this);
-        MobileCore.setLogLevel(LoggingMode.VERBOSE);
+        MobileCore.setLogLevel(LoggingMode.DEBUG);
 
         try {
             CampaignClassic.registerExtension();
+            UserProfile.registerExtension();
+            Identity.registerExtension();
             Lifecycle.registerExtension();
-            MobileCore.start(null);
+            Signal.registerExtension();
+            MobileCore.start(new AdobeCallback () {
+                @Override
+                public void call(Object o) {
+                    MobileCore.configureWithAppID("36817ad82b35/ff9a58fd45ca/launch-b54585ec721d");
+                }
+            });
         } catch (Exception e) {
             Log.e("CampaignClassicTestApp", e.getMessage());
         }
