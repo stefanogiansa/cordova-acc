e135ddfa8f9511e5a1812a3ff985ed55dadbcb8a
Stefano Giansante <giansante.stefano@konvergence.it>
2021-09-21 16:08:11 +0200

    override main activity

--- src/ios/ACC_Cordova.m
+++ src/ios/ACC_Cordova.m
@@ -13,6 +13,9 @@
 #import "ACPCore.h"
 #import "ACPCampaignClassic.h"
 #import "ACPLifecycle.h"
+#import "ACPUserProfile.h"
+#import "ACPIdentity.h"
+#import "ACPSignal.h"
 #import <Cordova/CDV.h>
 #import <Foundation/Foundation.h>
 #import "ACC_CordovaAppDelegate.h"
@@ -51,8 +54,18 @@
 }
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    [ACPCore setLogLevel:ACPMobileLogLevelDebug];
+    [ACPCore configureWithAppId:@"36817ad82b35/ff9a58fd45ca/launch-b54585ec721d"];
     [ACPCampaignClassic registerExtension];
+    [ACPUserProfile registerExtension];
+    [ACPIdentity registerExtension];
     [ACPLifecycle registerExtension];
+    [ACPSignal registerExtension];
+    [ACPCore start:^{
+        [ACPCore lifecycleStart:nil];
+        
+    }];
+    
     return YES;
 }
 
