3f084a81134e52c300c3fc0e45b468c857d052b0
Stefano Giansante <giansante.stefano@konvergence.it>
2021-09-21 12:37:01 +0200

    fix

--- src/android/KFirebaseMessagingService.java
+++ src/android/KFirebaseMessagingService.java
@@ -21,7 +21,6 @@ public class KFirebaseMessagingService extends FirebaseMessagingService {
     public void onNewToken(String token) {
         super.onNewToken(token);
         Log.d(TAG, "New token: " + token);
-        FCMPlugin.sendTokenRefresh(token);
     }
 
     /**
@@ -58,30 +57,5 @@ public class KFirebaseMessagingService extends FirebaseMessagingService {
         }
         
         Log.d(TAG, "\tNotification Data: " + data.toString());
-        FCMPlugin.sendPushPayload(data);
     }
-
-    @Override
-public void onResume() {
-    super.onResume();
-    // Perform any other app related tasks 
-    // The messageId (_mId) and deliveryId (_dId) can be passed in the intent extras.
-    // This is assuming you extract the messageId and deliveryId from the
-    // received push message and are including it in the intent (intent.putExtra())
-    // of the displayed notification.
-
-    Bundle extras = getIntent().getExtras();
-    if (extras != null) {
-        String deliveryId = extras.getString("_dId");
-        String messageId = extras.getString("_mId");
-        if (deliveryId != null && messageId != null) {
-            Map<String,String> trackInfo = new HashMap<>();
-            trackInfo.put("_mId", messageId);
-            trackInfo.put("_dId", deliveryId);
-
-            // Send the tracking information for message opening
-            CampaignClassic.trackNotificationClick(trackInfo);
-        }
-    }
-}
 }
